35731e4b47c04db58bf97185fda320e3f7fc5c81
fuhua <865358570@qq.com>
2023-06-03 12:40:25 +0800

    init

---
+++ spring-cloud-sleuth-2.2.0.RELEASE/spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/autoconfig/TraceAutoConfiguration.java
@@ -0,0 +1,321 @@
+/*
+ * Copyright 2013-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.sleuth.autoconfig;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import brave.CurrentSpanCustomizer;
+import brave.ErrorParser;
+import brave.Tracer;
+import brave.Tracing;
+import brave.TracingCustomizer;
+import brave.handler.FinishedSpanHandler;
+import brave.propagation.B3Propagation;
+import brave.propagation.CurrentTraceContext;
+import brave.propagation.CurrentTraceContextCustomizer;
+import brave.propagation.ExtraFieldCustomizer;
+import brave.propagation.ExtraFieldPropagation;
+import brave.propagation.Propagation;
+import brave.propagation.ThreadLocalCurrentTraceContext;
+import brave.sampler.Sampler;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import zipkin2.Span;
+import zipkin2.reporter.InMemoryReporterMetrics;
+import zipkin2.reporter.Reporter;
+import zipkin2.reporter.ReporterMetrics;
+import zipkin2.reporter.metrics.micrometer.MicrometerReporterMetrics;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.sleuth.DefaultSpanNamer;
+import org.springframework.cloud.sleuth.LocalServiceName;
+import org.springframework.cloud.sleuth.SpanAdjuster;
+import org.springframework.cloud.sleuth.SpanNamer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+
+/**
+ * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration
+ * Auto-configuration} to enable tracing via Spring Cloud Sleuth.
+ *
+ * @author Spencer Gibb
+ * @author Marcin Grzejszczak
+ * @author Tim Ysewyn
+ * @since 2.0.0
+ */
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnProperty(value = "spring.sleuth.enabled", matchIfMissing = true)
+@EnableConfigurationProperties(SleuthProperties.class)
+public class TraceAutoConfiguration {
+
+	/**
+	 * Tracer bean name. Name of the bean matters for some instrumentations.
+	 */
+	public static final String TRACER_BEAN_NAME = "tracer";
+
+	/**
+	 * Default value used for service name if none provided.
+	 */
+	public static final String DEFAULT_SERVICE_NAME = "default";
+
+	@Autowired(required = false)
+	List<SpanAdjuster> spanAdjusters = new ArrayList<>();
+
+	@Autowired(required = false)
+	List<FinishedSpanHandler> finishedSpanHandlers = new ArrayList<>();
+
+	@Autowired(required = false)
+	List<CurrentTraceContext.ScopeDecorator> scopeDecorators = new ArrayList<>();
+
+	@Autowired(required = false)
+	ExtraFieldPropagation.FactoryBuilder extraFieldPropagationFactoryBuilder;
+
+	@Autowired(required = false)
+	List<TracingCustomizer> tracingCustomizers = new ArrayList<>();
+
+	@Autowired(required = false)
+	List<CurrentTraceContextCustomizer> currentTraceContextCustomizers = new ArrayList<>();
+
+	@Autowired(required = false)
+	List<ExtraFieldCustomizer> extraFieldCustomizers = new ArrayList<>();
+
+	@Bean
+	@ConditionalOnMissingBean
+	// NOTE: stable bean name as might be used outside sleuth
+	Tracing tracing(@LocalServiceName String serviceName, Propagation.Factory factory,
+			CurrentTraceContext currentTraceContext, Sampler sampler,
+			ErrorParser errorParser, SleuthProperties sleuthProperties,
+			@Nullable List<Reporter<zipkin2.Span>> spanReporters) {
+		Tracing.Builder builder = Tracing.newBuilder().sampler(sampler)
+				.errorParser(errorParser)
+				.localServiceName(StringUtils.isEmpty(serviceName) ? DEFAULT_SERVICE_NAME
+						: serviceName)
+				.propagationFactory(factory).currentTraceContext(currentTraceContext)
+				.spanReporter(new CompositeReporter(this.spanAdjusters,
+						spanReporters != null ? spanReporters : Collections.emptyList()))
+				.traceId128Bit(sleuthProperties.isTraceId128())
+				.supportsJoin(sleuthProperties.isSupportsJoin());
+		for (FinishedSpanHandler finishedSpanHandlerFactory : this.finishedSpanHandlers) {
+			builder.addFinishedSpanHandler(finishedSpanHandlerFactory);
+		}
+		for (TracingCustomizer customizer : this.tracingCustomizers) {
+			customizer.customize(builder);
+		}
+		return builder.build();
+	}
+
+	@Bean(name = TRACER_BEAN_NAME)
+	@ConditionalOnMissingBean
+	Tracer tracer(Tracing tracing) {
+		return tracing.tracer();
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	Sampler sleuthTraceSampler() {
+		return Sampler.NEVER_SAMPLE;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	SpanNamer sleuthSpanNamer() {
+		return new DefaultSpanNamer();
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	Propagation.Factory sleuthPropagation(SleuthProperties sleuthProperties) {
+		if (sleuthProperties.getBaggageKeys().isEmpty()
+				&& sleuthProperties.getPropagationKeys().isEmpty()
+				&& extraFieldCustomizers.isEmpty()) {
+			return B3Propagation.FACTORY;
+		}
+		ExtraFieldPropagation.FactoryBuilder factoryBuilder;
+		if (this.extraFieldPropagationFactoryBuilder != null) {
+			factoryBuilder = this.extraFieldPropagationFactoryBuilder;
+		}
+		else {
+			factoryBuilder = ExtraFieldPropagation
+					.newFactoryBuilder(B3Propagation.FACTORY);
+		}
+		if (!sleuthProperties.getBaggageKeys().isEmpty()) {
+			factoryBuilder = factoryBuilder
+					// for HTTP
+					.addPrefixedFields("baggage-", sleuthProperties.getBaggageKeys())
+					// for messaging
+					.addPrefixedFields("baggage_", sleuthProperties.getBaggageKeys());
+		}
+		if (!sleuthProperties.getPropagationKeys().isEmpty()) {
+			for (String key : sleuthProperties.getPropagationKeys()) {
+				factoryBuilder = factoryBuilder.addField(key);
+			}
+		}
+		if (!sleuthProperties.getLocalKeys().isEmpty()) {
+			for (String key : sleuthProperties.getLocalKeys()) {
+				factoryBuilder = factoryBuilder.addRedactedField(key);
+			}
+		}
+		for (ExtraFieldCustomizer customizer : this.extraFieldCustomizers) {
+			customizer.customize(factoryBuilder);
+		}
+		return factoryBuilder.build();
+	}
+
+	@Bean
+	CurrentTraceContext sleuthCurrentTraceContext(CurrentTraceContext.Builder builder) {
+		for (CurrentTraceContext.ScopeDecorator scopeDecorator : this.scopeDecorators) {
+			builder.addScopeDecorator(scopeDecorator);
+		}
+		for (CurrentTraceContextCustomizer customizer : this.currentTraceContextCustomizers) {
+			customizer.customize(builder);
+		}
+		return builder.build();
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	CurrentTraceContext.Builder sleuthCurrentTraceContextBuilder() {
+		return ThreadLocalCurrentTraceContext.newBuilder();
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	Reporter<zipkin2.Span> noOpSpanReporter() {
+		return Reporter.NOOP;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	ErrorParser errorParser() {
+		return new ErrorParser();
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	// NOTE: stable bean name as might be used outside sleuth
+	CurrentSpanCustomizer spanCustomizer(Tracing tracing) {
+		return CurrentSpanCustomizer.create(tracing);
+	}
+
+	private static final class CompositeReporter implements Reporter<zipkin2.Span> {
+
+		private static final Log log = LogFactory.getLog(CompositeReporter.class);
+
+		private final List<SpanAdjuster> spanAdjusters;
+
+		private final Reporter<zipkin2.Span> spanReporter;
+
+		private CompositeReporter(List<SpanAdjuster> spanAdjusters,
+				List<Reporter<Span>> spanReporters) {
+			this.spanAdjusters = spanAdjusters;
+			this.spanReporter = spanReporters.size() == 1 ? spanReporters.get(0)
+					: new ListReporter(spanReporters);
+		}
+
+		@Override
+		public void report(Span span) {
+			Span spanToAdjust = span;
+			for (SpanAdjuster spanAdjuster : this.spanAdjusters) {
+				spanToAdjust = spanAdjuster.adjust(spanToAdjust);
+			}
+			this.spanReporter.report(spanToAdjust);
+		}
+
+		@Override
+		public String toString() {
+			return "CompositeReporter{" + "spanAdjusters=" + this.spanAdjusters
+					+ ", spanReporters=" + this.spanReporter + '}';
+		}
+
+		private static final class ListReporter implements Reporter<zipkin2.Span> {
+
+			private final List<Reporter<Span>> spanReporters;
+
+			private ListReporter(List<Reporter<Span>> spanReporters) {
+				this.spanReporters = spanReporters;
+			}
+
+			@Override
+			public void report(Span span) {
+				for (Reporter<zipkin2.Span> spanReporter : this.spanReporters) {
+					try {
+						spanReporter.report(span);
+					}
+					catch (Exception ex) {
+						log.warn("Exception occurred while trying to report the span "
+								+ span, ex);
+					}
+				}
+			}
+
+			@Override
+			public String toString() {
+				return "ListReporter{" + "spanReporters=" + this.spanReporters + '}';
+			}
+
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@ConditionalOnMissingClass("io.micrometer.core.instrument.MeterRegistry")
+	static class TraceMetricsInMemoryConfiguration {
+
+		@Bean
+		@ConditionalOnMissingBean
+		ReporterMetrics sleuthReporterMetrics() {
+			return new InMemoryReporterMetrics();
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@ConditionalOnClass(MeterRegistry.class)
+	static class TraceMetricsMicrometerConfiguration {
+
+		@Configuration(proxyBeanMethods = false)
+		@ConditionalOnMissingBean(ReporterMetrics.class)
+		static class NoReporterMetricsBeanConfiguration {
+
+			@Bean
+			@ConditionalOnBean(MeterRegistry.class)
+			ReporterMetrics sleuthMicrometerReporterMetrics(MeterRegistry meterRegistry) {
+				return MicrometerReporterMetrics.create(meterRegistry);
+			}
+
+			@Bean
+			@ConditionalOnMissingBean(MeterRegistry.class)
+			ReporterMetrics sleuthReporterMetrics() {
+				return new InMemoryReporterMetrics();
+			}
+
+		}
+
+	}
+
+}
---
+++ spring-cloud-sleuth-2.2.0.RELEASE/spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/log/Slf4jScopeDecorator.java
@@ -0,0 +1,238 @@
+/*
+ * Copyright 2013-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.sleuth.log;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import brave.internal.HexCodec;
+import brave.internal.Nullable;
+import brave.propagation.CurrentTraceContext;
+import brave.propagation.ExtraFieldPropagation;
+import brave.propagation.TraceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import org.springframework.cloud.sleuth.autoconfig.SleuthProperties;
+import org.springframework.util.StringUtils;
+
+/**
+ * Adds {@linkplain MDC} properties "traceId", "parentId", "spanId" and "spanExportable"
+ * when a {@link brave.Tracer#currentSpan() span is current}. These can be used in log
+ * correlation. Supports backward compatibility of MDC entries by adding legacy "X-B3"
+ * entries to MDC context "X-B3-TraceId", "X-B3-ParentSpanId", "X-B3-SpanId" and
+ * "X-B3-Sampled"
+ *
+ * @author Marcin Grzejszczak
+ * @since 2.1.0
+ */
+final class Slf4jScopeDecorator implements CurrentTraceContext.ScopeDecorator {
+
+	// Backward compatibility for all logging patterns
+	private static final String LEGACY_EXPORTABLE_NAME = "X-Span-Export";
+
+	private static final String LEGACY_PARENT_ID_NAME = "X-B3-ParentSpanId";
+
+	private static final String LEGACY_TRACE_ID_NAME = "X-B3-TraceId";
+
+	private static final String LEGACY_SPAN_ID_NAME = "X-B3-SpanId";
+
+	private static final Logger log = LoggerFactory.getLogger(Slf4jScopeDecorator.class);
+
+	private final SleuthProperties sleuthProperties;
+
+	private final SleuthSlf4jProperties sleuthSlf4jProperties;
+
+	Slf4jScopeDecorator(SleuthProperties sleuthProperties,
+			SleuthSlf4jProperties sleuthSlf4jProperties) {
+		this.sleuthProperties = sleuthProperties;
+		this.sleuthSlf4jProperties = sleuthSlf4jProperties;
+	}
+
+	static void replace(String key, @Nullable String value) {
+		if (value != null) {
+			MDC.put(key, value);
+		}
+		else {
+			MDC.remove(key);
+		}
+	}
+
+	@Override
+	public CurrentTraceContext.Scope decorateScope(TraceContext currentSpan,
+			CurrentTraceContext.Scope scope) {
+		final String previousTraceId = MDC.get("traceId");
+		final String previousParentId = MDC.get("parentId");
+		final String previousSpanId = MDC.get("spanId");
+		final String spanExportable = MDC.get("spanExportable");
+		final String legacyPreviousTraceId = MDC.get(LEGACY_TRACE_ID_NAME);
+		final String legacyPreviousParentId = MDC.get(LEGACY_PARENT_ID_NAME);
+		final String legacyPreviousSpanId = MDC.get(LEGACY_SPAN_ID_NAME);
+		final String legacySpanExportable = MDC.get(LEGACY_EXPORTABLE_NAME);
+		final List<AbstractMap.SimpleEntry<String, String>> previousMdc = previousMdc();
+
+		if (currentSpan != null) {
+			String traceIdString = currentSpan.traceIdString();
+			MDC.put("traceId", traceIdString);
+			MDC.put(LEGACY_TRACE_ID_NAME, traceIdString);
+			String parentId = currentSpan.parentId() != null
+					? HexCodec.toLowerHex(currentSpan.parentId()) : null;
+			replace("parentId", parentId);
+			replace(LEGACY_PARENT_ID_NAME, parentId);
+			String spanId = HexCodec.toLowerHex(currentSpan.spanId());
+			MDC.put("spanId", spanId);
+			MDC.put(LEGACY_SPAN_ID_NAME, spanId);
+			String sampled = String.valueOf(currentSpan.sampled());
+			MDC.put("spanExportable", sampled);
+			MDC.put(LEGACY_EXPORTABLE_NAME, sampled);
+			log("Starting scope for span: {}", currentSpan);
+			if (currentSpan.parentId() != null) {
+				if (log.isTraceEnabled()) {
+					log.trace("With parent: {}", currentSpan.parentId());
+				}
+			}
+			for (String key : whitelistedBaggageKeysWithValue(currentSpan)) {
+				MDC.put(key, ExtraFieldPropagation.get(currentSpan, key));
+			}
+			for (String key : whitelistedPropagationKeysWithValue(currentSpan)) {
+				MDC.put(key, ExtraFieldPropagation.get(currentSpan, key));
+			}
+			for (String key : whitelistedLocalKeysWithValue(currentSpan)) {
+				MDC.put(key, ExtraFieldPropagation.get(currentSpan, key));
+			}
+		}
+		else {
+			MDC.remove("traceId");
+			MDC.remove("parentId");
+			MDC.remove("spanId");
+			MDC.remove("spanExportable");
+			MDC.remove(LEGACY_TRACE_ID_NAME);
+			MDC.remove(LEGACY_PARENT_ID_NAME);
+			MDC.remove(LEGACY_SPAN_ID_NAME);
+			MDC.remove(LEGACY_EXPORTABLE_NAME);
+			for (String s : whitelistedBaggageKeys()) {
+				MDC.remove(s);
+			}
+			for (String s : whitelistedPropagationKeys()) {
+				MDC.remove(s);
+			}
+			for (String s : whitelistedLocalKeys()) {
+				MDC.remove(s);
+			}
+			previousMdc.clear();
+		}
+
+		/**
+		 * Thread context scope.
+		 *
+		 * @author Adrian Cole
+		 */
+		class ThreadContextCurrentTraceContextScope implements CurrentTraceContext.Scope {
+
+			@Override
+			public void close() {
+				log("Closing scope for span: {}", currentSpan);
+				scope.close();
+				replace("traceId", previousTraceId);
+				replace("parentId", previousParentId);
+				replace("spanId", previousSpanId);
+				replace("spanExportable", spanExportable);
+				replace(LEGACY_TRACE_ID_NAME, legacyPreviousTraceId);
+				replace(LEGACY_PARENT_ID_NAME, legacyPreviousParentId);
+				replace(LEGACY_SPAN_ID_NAME, legacyPreviousSpanId);
+				replace(LEGACY_EXPORTABLE_NAME, legacySpanExportable);
+				for (AbstractMap.SimpleEntry<String, String> entry : previousMdc) {
+					replace(entry.getKey(), entry.getValue());
+				}
+			}
+
+		}
+		return new ThreadContextCurrentTraceContextScope();
+	}
+
+	private List<AbstractMap.SimpleEntry<String, String>> previousMdc() {
+		List<AbstractMap.SimpleEntry<String, String>> previousMdc = new ArrayList<>();
+		List<String> keys = new ArrayList<>(whitelistedBaggageKeys());
+		keys.addAll(whitelistedPropagationKeys());
+		keys.addAll(whitelistedLocalKeys());
+		for (String key : keys) {
+			previousMdc.add(new AbstractMap.SimpleEntry<>(key, MDC.get(key)));
+		}
+		return previousMdc;
+	}
+
+	private List<String> whitelistedKeys(List<String> keysToFilter) {
+		List<String> keys = new ArrayList<>();
+		for (String baggageKey : keysToFilter) {
+			if (this.sleuthSlf4jProperties.getWhitelistedMdcKeys().contains(baggageKey)) {
+				keys.add(baggageKey);
+			}
+		}
+		return keys;
+	}
+
+	private List<String> whitelistedBaggageKeys() {
+		return whitelistedKeys(this.sleuthProperties.getBaggageKeys());
+	}
+
+	private List<String> whitelistedKeysWithValue(TraceContext context,
+			List<String> keys) {
+		if (context == null) {
+			return Collections.EMPTY_LIST;
+		}
+		List<String> nonEmpty = new ArrayList<>();
+		for (String key : keys) {
+			if (StringUtils.hasText(ExtraFieldPropagation.get(context, key))) {
+				nonEmpty.add(key);
+			}
+		}
+		return nonEmpty;
+	}
+
+	private List<String> whitelistedBaggageKeysWithValue(TraceContext context) {
+		return whitelistedKeysWithValue(context, whitelistedBaggageKeys());
+	}
+
+	private List<String> whitelistedPropagationKeys() {
+		return whitelistedKeys(this.sleuthProperties.getPropagationKeys());
+	}
+
+	private List<String> whitelistedLocalKeys() {
+		return whitelistedKeys(this.sleuthProperties.getLocalKeys());
+	}
+
+	private List<String> whitelistedPropagationKeysWithValue(TraceContext context) {
+		return whitelistedKeysWithValue(context, whitelistedPropagationKeys());
+	}
+
+	private List<String> whitelistedLocalKeysWithValue(TraceContext context) {
+		return whitelistedKeysWithValue(context, whitelistedLocalKeys());
+	}
+
+	private void log(String text, TraceContext span) {
+		if (span == null) {
+			return;
+		}
+		if (log.isTraceEnabled()) {
+			log.trace(text, span);
+		}
+	}
+
+}
